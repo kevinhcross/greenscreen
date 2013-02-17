536dab7c91d1415df3afa87cb0cace7991031800
Kevin Cross <kevinhcross@gmail.com>
2013-02-17 21:41:48 +0000

    this is working from the json now but is still pretty basic.

--- greenscreen.rb
+++ greenscreen.rb
@@ -5,49 +5,71 @@ require 'rexml/document'
 require 'hpricot'
 require 'open-uri'
 require 'yaml'
+require 'json'
+require 'net/http'
+
+def get_data(url)
+  resp = Net::HTTP.get_response(URI.parse(url))
+  data = resp.body
+end
+
+# Parse the json output from Jenkins to create job objects
+def get_jobs(url)
+  result = JSON.parse(get_data(url))
+  job_list = []
+  result["jobs"].each do |job_data|
+
+    healthReport = job_data["healthReport"][0]
+    score = healthReport ? healthReport["score"] : -1
+
+    job = JenkinsJob.new job_data["name"], job_data["color"], job_data["url"], score
+    job_list << job
+  end
+  job_list
+end
+
+# We are only interested in failing jobs and also we want them sorted
+def sort_jobs(job_list)
+  sorted_and_filtered = []
+  [/red.*/, /yellow.*/, /grey.*/].each do |color|
+    job_list.select {|j| j.color =~ color}.each { |j| sorted_and_filtered << j }
+  end
+  sorted_and_filtered
+end
 
 get '/' do
   servers = YAML.load_file 'config.yml'
   return "Add the details of build server to the config.yml file to get started" unless servers
-  puts "there again"
   
-  @projects = []
+  @sorted_job_list = []
 
   servers.each do |server|
-    xml = REXML::Document.new(open(server["url"], :http_basic_authentication=>[server["username"], server["password"]]))
-    projects = xml.elements["//Projects"]
-    
-    projects.each do |project|
-      monitored_project = MonitoredProject.new(project)
-      if server["jobs"]
-        if server["jobs"].detect {|job| job == monitored_project.name}
-          @projects << monitored_project
-        end
-      else
-        @projects << monitored_project
-      end
-    end
+    url = server["url"] + "/api/json?depth=1"
+    puts "Getting data from #{url}"
+    @sorted_job_list = sort_jobs(get_jobs(url))
+    @sorted_job_list.each { |j| puts j }
   end
 
   @columns = 1.0
-  @columns = 2.0 if @projects.size > 4
-  @columns = 3.0 if @projects.size > 10
-  @columns = 4.0 if @projects.size > 21
+  @columns = 2.0 if @sorted_job_list.size > 4
+  @columns = 3.0 if @sorted_job_list.size > 10
+  @columns = 4.0 if @sorted_job_list.size > 21
   
-  @rows = (@projects.size / @columns).ceil
+  @rows = (@sorted_job_list.size / @columns).ceil
 
   erb :index
 end
 
-class MonitoredProject
-  attr_reader :name, :last_build_status, :activity, :last_build_time, :web_url, :last_build_label
-  
-  def initialize(project)
-    @activity = project.attributes["activity"]
-    @last_build_time = Time.parse(project.attributes["lastBuildTime"]).localtime
-    @web_url = project.attributes["webUrl"]
-    @last_build_label = project.attributes["lastBuildLabel"]
-    @last_build_status = project.attributes["lastBuildStatus"].downcase
-    @name = project.attributes["name"]
+class JenkinsJob
+  attr_accessor :name, :color, :url, :health
+  def initialize(name, color, url, health)
+    @name = name
+    @color = color
+    @url = url
+    @health = health
+  end
+  def to_s
+    "#{@name}\t#{@color}\t#{@health}\t#{@url}"
   end
 end
+
--- read_json.rb
+++ read_json.rb
@@ -13,27 +13,32 @@ class JenkinsJob
   end
 end
 
+def get_data(url)
+  resp = Net::HTTP.get_response(URI.parse(url))
+  data = resp.body
+end
 
+# Parse the json output from Jenkins to create job objects
+def get_jobs(url)
+  result = JSON.parse(get_data(url))
+  job_list = []
+  result["jobs"].each do |job|
+    job = JenkinsJob.new job["name"], job["color"], job["url"]
+    job_list << job
+  end
+  job_list
+end
+
+# We are only interested in failing jobs and also we want them sorted
+def sort_jobs(job_list)
+  sorted_and_filtered = []
+  ["red", "yellow", "grey"].each do |color|
+    job_list.select {|j| j.color == color}.each { |j| sorted_and_filtered << j }
+  end
+  sorted_and_filtered
+end
 
 url = "http://localhost:8080/api/json"
-resp = Net::HTTP.get_response(URI.parse(url))
-data = resp.body
-
-# we convert the returned JSON data to native Ruby
-# data structure - a hash
-result = JSON.parse(data)
-job_list = []
-
-result["jobs"].each do |job|
-  job = JenkinsJob.new job["name"], job["color"], job["url"]
-  job_list << job
-end
-
-sorted_and_filtered = []
-
-["red", "yellow", "grey"].each do |color|
-  job_list.select {|j| j.color == color}.each { |j| sorted_and_filtered << j }
-end
-
-sorted_and_filtered.each { |j| puts j }
+sorted_job_list = sort_jobs(get_jobs(url))
+sorted_job_list.each { |j| puts j }
 
